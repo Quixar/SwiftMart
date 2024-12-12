bd797aab0cea38d2f4759294716d8e7972a4484d
Mukhammed Lolo <lmecomcompany@gmail.com>
2024-12-12 11:47:55 +0100

    New Entities Have Been Added

--- SwiftMart/ProductEntity/Product.cs
+++ SwiftMart/ProductEntity/Product.cs
@@ -1,6 +1,16 @@
-﻿namespace SwiftMart.Products
+﻿using SwiftMart.CategoryEntity;
+using SwiftMart.ReviewEntity;
+
+namespace SwiftMart.Products
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        //public Category Category { get; set; } -- TO DO
+        public Review Review { get; set; }
+
     }
 }
